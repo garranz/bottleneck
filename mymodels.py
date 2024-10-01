cf39a85caf11ff50b43a68d2b77f3a28ba093eda
garranz <garranz@mit.edu>
2024-10-01 09:19:39 -0400

    add myversion of DIB (wip)

---
+++ mymodels.py
@@ -0,0 +1,122 @@
+import torch as tc
+import torch.nn as nn
+
+def mlp( din: int, dout: int, arch: tuple, fn_act ):
+
+    # Input Layer
+    layers = []
+
+    ## Positional Encoding
+    #if use_positional_encoding:
+    #    layers.append(PositionalEncoding(positional_encoding_frequencies))
+
+    # Hidden Layers
+    N0 = din
+    for N1 in arch:
+        layers += [ nn.Linear( N0, N1 ), fn_act() ]
+        N0 = N1
+
+    layers += [ nn.Linear( N0, dout ) ]
+
+    return nn.Sequential(*layers) 
+
+
+class DIBnet( nn.Module ):
+
+    def __init__( self, in_dims: tuple, out_dim: int, Earch: tuple= (8,8), 
+                 in_emb_d: int= 32, Oarch: tuple= (8,4), 
+                 out_fn_act = nn.ReLU ):
+        '''
+        Parameters
+            in_dims     list | tuple (ints)
+                the i-entry correspond to the number of dimensions of the
+                i-input. Thus, len( in_dims ) is the number of inputs.
+
+            out_dim:    int
+                number of output dimensions
+
+            Earch       list | tuple (ints) (*(8,8))
+                Architecture of the encoder. Assume all encoders are the same
+                for all input. List with the number of neurons 
+                for each layer. e.g.: an encoder with 12 neurons in the first
+                layer and 24 in the second layer -> (12,24)
+
+            in_emb_d    int (*32)
+                embedding space dimensionality for each input (same for each
+                feature)
+
+            Oarch       list | tuple (ints) (*(8,4))
+                Architecture of the mlp that tries to reconstruct the mutual
+                information with the output. List with the number of neurons 
+                for each layer. e.g.: an encoder with 12 neurons in the first
+                layer and 24 in the second layer -> (12,24)
+
+            out_fn_act  nn.modules.activation   (*nn.ReLU)
+                Activation function for the final output layer
+        '''
+        super(DIBnet, self).__init__()  # Calling the parent class constructor
+
+        self.in_dims = in_dims       # number of dimensions for each input
+        self.Nin = len( in_dims )    # number of inputs
+        self.in_emb_d = in_emb_d     # number of embedded dimension of input
+
+        ########################################################################
+        # Create encoders for each input
+        ########################################################################
+        fn_act = nn.ReLU    # Activation function
+
+        Elist = []
+        for in_d in self.in_dims:
+
+            # NOTE: The last layer returns the means and stds of p(u|x), so the 
+            # number of outputs is 2*in_emb_d
+            Elist.append( mlp( in_d, 2*self.in_emb_d, Earch, fn_act ) )
+
+        self.Elist = Elist # list of encoders (one for each input)
+
+        ########################################################################
+        # Create integration network
+        ########################################################################
+        self.OutNet = mlp( self.in_emb_d*self.Nin, out_dim, Oarch, fn_act )
+        self.OutNet.append( out_fn_act() )
+
+
+    def forward(self, inputs: tuple ):
+        '''
+        Parameters
+            inputs      tuple
+                Dimension: self.Nin. The i-item in the list is a tc.Tensor 
+                of size (Nsmaples, self.in_dims[i])
+
+        Returns
+            tc.tensor   ( Nsamples, out_dim )
+                Prediction from the outer net using the input embeddings
+
+            tuple       ( Nin, )
+                average KL for each input
+        '''
+        emb_ch_all = []
+        kl_all = []
+
+        # For each input, apply the enconder
+        for k,inp in enumerate(inputs):
+            # Split the output of the encoder into the mean and logvar of each
+            # embeded feature -> (mu, log[std**2] )
+            emb_mu, emb_logvar = tc.split( self.Elist[k](inp), 
+                                          self.in_emb_d, dim=-1)
+
+            # For each sample (mu,std), return a random number that follows that
+            # normal distribution
+            emb_ch = tc.normal(mean=emb_mu, std=tc.exp(emb_logvar/2.))
+            # NOTE -> emb_ch.shape = ( Nsamples, in_emb_d )
+            emb_ch_all.append( emb_ch )
+
+            # Compute the KL wrt N(0,1) for every sample, then take the average:
+            kl = tc.mean( 0.5*(tc.square(emb_mu) + 
+                tc.exp(emb_logvar) - emb_logvar - 1.) )
+            kl_all.append( kl )
+
+        # The input for OutNet must be ( Nsamples, in_emb_d*Nin )
+        return self.OutNet( tc.cat( emb_ch_all, -1 ) ), kl_all
+
+
---
+++ myutils.py
@@ -0,0 +1,55 @@
+import torch as tc
+
+def MIsandwich( mus: tc.Tensor, logvars: tc.Tensor ):
+    '''
+    Paramters
+        mus         tc.Tensor (Nbatch, in_dim)
+            posterior mean -> output from the enconder of a given input
+
+        logvars     tc.Tensor (Nbatch, in_dim)
+            posterior logvar -> output from the enconder of a given input
+
+    Returns
+    '''
+
+    # Convert varibales to double for extra precision
+    mus_d, logvars_d = mus.double(), logvars.double()
+    stds_d = tc.exp(logvars_d/2.)
+
+    u_samples = tc.normal(mean=mus_d, std=stds_d )
+
+    # Compute pairwise distance between sample points ant the centers of the
+    # conditional distributions
+    # (Nsamples,Nsamples,in_dim) = (Nsamples,1,in_dim) - (1,Nsamples,in_dim)
+    d_ui_muj = u_samples.unsqueeze(1) - mus_d.unsqueeze(0)
+    d_ui_muj /= stds_d.unsqueeze(0)  # Normalize by std
+
+    p_ui_xj = tc.exp(tc.sum(d_ui_muj**2,dim=-1)/2. - 
+        tc.sum(logvars_d,dim=-1).unsqueeze(0)/2.)
+    # NOTE: for a multivariate gaussian distribution:
+    # p(ui| muj, sj ) = 1/ [ (2pi)^(k/2) * |S|^1/2 ] * exp( 1/2 * d_ui_muj^2 )
+    # the last term in p_ui_xj comes from inserting |S|^1/2 into the exponent
+    p_ui_xj /= (2.*tc.pi)**( mus_d.size(-1) / 2. )
+
+
+    ############################################################################
+    # InfoNCE (lower bound)
+    ############################################################################
+    # Extract diagonal elements (p_ui_xi)
+    p_ui_xi = tc.diagonal(p_ui_xj, dim1=-2, dim2=-1)
+
+    # Compute the InfoNCE lower bound
+    infonce_lower = tc.mean( tc.log(p_ui_xi / tc.mean(p_ui_xj,dim=1)) )
+
+
+    ############################################################################
+    # Leave 1 Out (upper bound)
+    ############################################################################
+    # Zero out diagonal elements of p_ui_cond_xj
+    I = tc.eye(mus_d.size(0), dtype=tc.double, device=mus.device)
+    p_ui_xj *= (1. - I)
+
+    # Compute the Leave-One-Out upper bound
+    l1o_upper = tc.mean(tc.log(p_ui_xi / tc.mean(p_ui_xj, dim=1)))
+
+    return infonce_lower, l1o_upper
